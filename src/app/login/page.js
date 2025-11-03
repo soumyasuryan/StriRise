477ea7f360065619f8b0ce58cab9ed308a3151a0
Soumya Suryan <soumyasuryan86@gmail.com>
2025-11-03 15:25:55 +0530

    signup login url Modified

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -20,7 +20,7 @@ export default function LoginPage() {
   setLoading(true);
 
   try {
-    const res = await fetch("http://localhost:5000/login", {
+    const res = await fetch("https://stririsebackend.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -29,7 +29,7 @@ export default function SignupPage() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/signup", {
+      const res = await fetch("https://stririsebackend.onrender.com/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
